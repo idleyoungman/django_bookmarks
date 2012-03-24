6b6a2c42113dbf085599a208f9b412a799cd6d27
Dan Young <danielmyoung@gmail.com>
2012-03-24 16:05:12 -0700

    add main_page() to views

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from django.http import HttpResponse
+
+from main_page(request):
+	output = u'''
+		<html>
+			<head><title>%s</title></html>
+			<body>
+				<h1>%s</h1><p>%s</p>
+			</body>
+		</html>
+	''' % (
+		u'Django Bookmarks',
+		u'Welcome to Django Bookmarks',
+		u'Where you can store and share bookmarks!'
+	)
+	return HttpResponse(output)
