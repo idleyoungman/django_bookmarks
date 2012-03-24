7d930326fbfc058921149ff3bab294fabd422112
Dan Young <danielmyoung@gmail.com>
2012-03-24 16:10:38 -0700

    add main_page to urlpatterns

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from bookmarks.views import *
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -14,4 +15,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    
+    (r'^$', main_page),
+    
 )
