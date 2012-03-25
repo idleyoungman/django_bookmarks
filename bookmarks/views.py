96b4e7b3a48ef2c1787c679e1373e0ec73db63ef
Dan Young <danielmyoung@gmail.com>
2012-03-24 18:43:55 -0700

    add user_page() to views

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.contrib.auth.models import User
 from django.template import Context
 from django.template.loader import get_template
 
@@ -8,6 +9,20 @@ def main_page(request):
 		'head_title': u'Django Bookmarks',
 		'page_title': u'Welcome to Django Bookmarks',
 		'page_body': u'Where you can store and share bookmarks!'
+	})
+	output = template.render(variables)
+	return HttpResponse(output)
+
+def user_page(request, username):
+	try:
+		user = User.objects.get(username=username)
+	except User.DoesNotExist:
+		raise Http404(u'Requested user not found')
+	bookmarks = user.bookmark_set.all()
+	template = get_template('user_page.html')
+	variables = Context({
+		'username': username,
+		'bookmarks': bookmarks
 	})
 	output = template.render(variables)
 	return HttpResponse(output)
