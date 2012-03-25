fff7f1edc0278661f23f1583c5abba0234ba8181
Dan Young <danielmyoung@gmail.com>
2012-03-25 16:01:43 -0700

    send user to main_page; don't use variables for page content

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -5,11 +5,7 @@ from django.template.loader import get_template
 
 def main_page(request):
 	template = get_template('main_page.html')
-	variables = Context({
-		'head_title': u'Django Bookmarks',
-		'page_title': u'Welcome to Django Bookmarks',
-		'page_body': u'Where you can store and share bookmarks!'
-	})
+	variables = Context({'user': request.user})
 	output = template.render(variables)
 	return HttpResponse(output)
 
