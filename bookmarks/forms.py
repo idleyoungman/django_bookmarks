e5e9bf977dd8cc05491033dc41f4cccb0b05b9f0
Dan Young <danielmyoung@gmail.com>
2012-04-01 18:55:53 -0700

    add password validation

--- bookmarks/forms.py
+++ bookmarks/forms.py
@@ -11,3 +11,11 @@ class RegistrationForm(forms.Form):
 		label = u'Password (Again)',
 		widget = forms.PasswordInput()
 	)
+
+	def clean_password2(self):
+		if 'password1' in self.cleaned_data:
+			password1 = self.cleaned_data['password1']
+			password2 = self.cleaned_data['password2']
+			if password1 == password2:
+				return password2
+		raise forms.ValidationError('Passwords do not match.')
