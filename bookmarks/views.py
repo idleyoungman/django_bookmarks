9e57a990c84074879031f2e18a7f5ebb13af1ebf
Dan Young <danielmyoung@gmail.com>
2012-04-01 21:40:16 -0700

    add register_page function to views

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,3 +1,5 @@
+from bookmarks.forms import *
+
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.contrib.auth import logout
 from django.contrib.auth.models import User
@@ -9,6 +11,26 @@ def main_page(request):
 	return render_to_response(
 		'main_page.html',
 		{'user': request.user}
+	)
+
+def register_page(request):
+	if request.method == 'POST':
+		form = RegistrationForm(request.POST)
+		if form.is_valid():
+			user = User.objects.create_user(
+				username = form.cleaned_data['username'],
+				password = form.cleaned_data['password1'],
+				email = form.cleaned_data['email']
+			)
+			return HttpResponseRedirect('/')
+	else:
+		form = RegistrationForm()
+	variables = RequestContext(request, {
+		'form': form
+	})
+	return render_to_response(
+		'registration/register.html',
+		variables
 	)
 
 def user_page(request, username):
