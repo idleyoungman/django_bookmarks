3783d01d69c91fd960ebcbb831f342387f1d83d0
Dan Young <danielmyoung@gmail.com>
2012-05-20 14:57:19 -0700

    Add initial form for adding a bookmark

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -3,6 +3,7 @@ from bookmarks.forms import *
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.contrib.auth import logout
 from django.contrib.auth.models import User
+from django.models import *
 from django.template import Context, RequestContext
 from django.template.loader import get_template
 from django.shortcuts import render_to_response
@@ -52,3 +53,39 @@ def user_page(request, username):
 def logout_page(request):
     logout(request)
     return HttpResponseRedirect('/')
+
+
+def bookmark_save_page(request):
+    if request.method == 'POST':
+        form = BookmarkSaveForm(request.POST)
+        if form.is_valid():
+            # Create or get link
+            link, dummy = Link.objects.get_or_create(
+                    url=form.cleaned_data['url']
+            )
+            # Create or get bookmarks
+            bookmark, created = Bookmark.objects.get_or_create(
+                    url=request.user,
+                    link=link
+            )
+            # Update bookmark title
+            bookmark.title = form.cleaned_data['title']
+            # If the bookmark is being updates, clear old tag list
+            if not created:
+                bookmark.tag_set.clear()
+            # Create new tag list
+            tag_name = form.cleaned_data['tags'].split()
+            for tag_name in tag_names:
+                tag, dummy = Tag.objects.get_or_create(name=tag_name)
+                bookmark.tag_set.add(tag)
+            # Save bookmark to database
+            bookmark.save()
+            return HttpResponseRedirect(
+                '/user/%s/' % request.user.username
+            )
+        else:
+            form = BookmarkSaveForm()
+        variables = RequestContext(request, {
+            'form': form
+        })
+        return render_to_response('bookmark_save.html', variables)
