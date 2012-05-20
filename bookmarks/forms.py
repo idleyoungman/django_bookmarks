db415df244625cca7d63ad2efb8aa796d2bf6734
Dan Young <danielmyoung@gmail.com>
2012-05-20 14:43:15 -0700

    Add initial bookmark form model

--- bookmarks/forms.py
+++ bookmarks/forms.py
@@ -33,3 +33,19 @@ class RegistrationForm(forms.Form):
         except User.DoesNotExist:
             return username
         raise forms.ValidationError('Username is already taken.')
+
+
+class BookmarkSaveForm(forms.Form):
+    url = forms.URLField(
+            label=u'URL',
+            widget=forms.TextInput(attrs={'size': 64})
+    )
+    title = forms.CharField(
+            label=u'Title',
+            widget=forms.TextInput(attrs={'size': 64})
+    )
+    tags = forms.CharField(
+            label=u'Tags',
+            required=False,
+            widget=forms.TextInput(attrs={'size': 64})
+    )
